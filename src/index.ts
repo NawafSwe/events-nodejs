1906338babb97945c35fdaeb3c0848430f6388a0
NawafSwe <nawaf.jr9@hotmail.com>
2021-04-04 08:39:50 +0300

    simple code to show event/emit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {EventEmitter} from 'events';
+// we can use options for events { - }
+const ee = new EventEmitter();
+//To publish an event, we use the emit() function, and to listen to an event, we use the on() function.
+
+// listen to the event 
+ee.on(`myEvent`, ()=>{
+    console.log(` data received`);
+});
+
+//publishing the event simply as 
+ee.emit(`myEvent`);
