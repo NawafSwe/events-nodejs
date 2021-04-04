926a6f8df36c39b9d1656c36afea8a3627cef26b
NawafSwe <nawaf.jr9@hotmail.com>
2021-04-04 08:46:20 +0300

    changing way of import files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {EventEmitter} from 'events';
+const EventEmitter = require(`events`);
 // we can use options for events { - }
 const ee = new EventEmitter();
 //To publish an event, we use the emit() function, and to listen to an event, we use the on() function.
