67f18a8ae51ab871f69caf509d7994fa05659fb1
NawafSwe <nawaf.jr9@hotmail.com>
2021-04-04 08:54:45 +0300

    including express to deal with events

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 const EventEmitter = require(`events`);
 // we can use options for events { - }
 const ee = new EventEmitter();
+const express = require(`express`);
+const app = express();
 //To publish an event, we use the emit() function, and to listen to an event, we use the on() function.
 
 // listen to the event 
@@ -10,3 +12,8 @@ ee.on(`myEvent`, ()=>{
 
 //publishing the event simply as 
 ee.emit(`myEvent`);
+
+// if we want to use and manage the events across the whole app we can use 
+app.set('ee', ee);
+// access it from any module of the application
+console.log(app.get('ee'));
